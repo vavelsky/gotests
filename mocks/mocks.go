069b9922bc76027b915bd7b0946cec1d0be24ee8
vavelsky <smock.vavelsky@gmail.com>
2020-11-15 13:59:20 +0100

    Added sleepers

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -10,16 +10,36 @@ import (
 const finalWord = "Go!"
 const startCount = 3
 
+// Sleeper allows to put delays
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct{}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 // Countdown counts from the number to zero and prints Go!
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := startCount; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
